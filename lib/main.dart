52b0affd5b74bf07cd1b63f61102a5c77a37c1a4
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2026-05-11 12:09:27 +0530

    chore: added app logo and bluetooth support and udpated app name  that is shown on the launcher

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,21 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Hello World!')));
   }
 }
