390ffc3b0ee2d0fcb130837c0fc49c2431f65376
tech explorers <73522283+techexplorers123@users.noreply.github.com>
2026-05-11 14:41:48 +0530

    feat: added speech lib and code

---
+++ lib/services/speech_service.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class SpeechService {
+  final FlutterTts tts = FlutterTts();
+
+  Future<void> init() async {
+    await tts.setLanguage("en-US");
+
+    await tts.setSpeechRate(0.5);
+
+    await tts.setPitch(1.0);
+
+    await tts.awaitSpeakCompletion(true);
+  }
+
+  Future<void> speak(String text, {bool interrupt = false}) async {
+    if (text.trim().isEmpty) {
+      return;
+    }
+
+    if (interrupt) {
+      await tts.stop();
+    }
+
+    await tts.speak(text);
+  }
+
+  Future<void> stop() async {
+    await tts.stop();
+  }
+}
